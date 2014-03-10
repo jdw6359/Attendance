b8654bd1a9b18a51349d5da63b3fd45d945fd063
Josh Woodward <josh_woodward@medisked.com>
2014-03-10 14:29:01 -0400

    Created hash function rather than use python's due to lack of consistency in function

--- attendance.py
+++ attendance.py
@@ -1,21 +1,46 @@
-def record_brother(ID):
-    print("Brother with ID: "+str(ID)+" in attendance")
-    
+
+
+#construct hashmap mapping brothers hashed UID to their name
+def init_list():
+    brothers=open("names_ids.txt")
+    #create mapping between id and brother
+    id_name_map={}
+    for pairing in brothers:
+        pair=pairing.split(":")
+        UID=pair[1].replace("\n", "")
+        NAME=pair[0]
+        #add to map
+        id_name_map[UID]=NAME
+    return id_name_map
+
+def hash_func(id):
+    multiplier=1
+    hash_value=0
+    for num in id:
+        print(str(int(num)))
+        hash_value+=pow(int(num), multiplier)
+        
+        multiplier+=1
+
+    return hash_value
 
 
 
-
-
+#main method
 def main():
     print("Event Starting\n")
 
-    """
-        attendees=["Woodward", "Kenny", "Fink"]
-        for attendee in attendees:
-            record_brother(attendee)
-    """
+    #id_name_map is the mapping between a brothers UID and their name
+    id_name_map=init_list()
+    print(id_name_map)
+
+
     while(1):
+        #recieve
         brother_id=input()
-        record_brother(brother_id)
+        #strip issue number off of id
+        brother_id=brother_id[:9]
+        print(brother_id)
+        print(hash_func(brother_id))
 
 main()
--- scan_to_name.py
+++ scan_to_name.py
@@ -1,20 +1,33 @@
-f=open('names_ids.txt','a')
-print("File opened")
 
-while(1):
-        option=input("Press \'y\' to add new brother.\nAny other input will cancel")
-        if(option=='y'):
-                brother_name=input("Brother Name: ")
-                brother_id=input("Please Scan RIT ID")
-                brother_id=brother_id[:9]
-                brother_hash=hash(brother_id)
-                print("University ID: "+str(brother_id))
-                f.write(str(brother_name)+":"+str(brother_hash)+'\n')
-                print(brother_name+" added to file\n\n")
-        else:
-                print("closing program")
-                f.close()
-                break
-                
-        
+
+def hash_func(id):
+    multiplier=1
+    hash_value=0
+    for num in id:
+        hash_value+=pow(int(num), multiplier)
+        multiplier+=1
+    return hash_value
+
+def main():
+        f=open('names_ids.txt','a')
+        print("File opened")
+
+
+
+        while(1):
+                option=input("Press \'y\' to add new brother.\nAny other input will cancel")
+                if(option=='y'):
+                        brother_name=input("Brother Name: ")
+                        brother_id=input("Please Scan RIT ID")
+                        brother_id=brother_id[:9]
+                        brother_hash=hash_func(brother_id)
+                        print("University ID: "+str(brother_id))
+                        f.write(str(brother_name)+":"+str(brother_hash)+'\n')
+                        print(brother_name+" added to file\n\n")
+                else:
+                        print("closing program")
+                        f.close()
+                        break
+                        
+main()        
         
